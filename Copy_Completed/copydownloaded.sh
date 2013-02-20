6dbc2439472fd61f1b274f7409dfd80381faf880
John Buluba <buluba89@gmail.com>
2013-02-20 19:46:24 +0200

    wrong file

--- Copy_Completed/copydownloaded.sh
+++ Copy_Completed/copydownloaded.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-env >> /home/john/transmission.txt
+COMPLETED_PATH="/mnt/pool/Data/Torrents/Completed"
+
+#TERM=linux
+#TR_TORRENT_DIR=/mnt/pool/Data/Torrents/Data
+#TR_TIME_LOCALTIME=Wed Feb 20 14:50:31 2013
+#TR_TORRENT_ID=7
+#PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/sbin
+#PWD=/
+#TR_TORRENT_HASH=d29e01b1524e8066428246d2fc8e57ab7ecc6610
+#LANG=
+#SHLVL=1
+#HOME=/home/debian-transmission
+#TR_APP_VERSION=2.76
+#TR_TORRENT_NAME=The Complete Idiot's Guide to Simple Home Repair -Mantesh
+#_=/usr/bin/env
+
+#TR_TORRENT_NAME="The Complete Idiot's Guide to Simple Home Repair -Mantesh"
+#TR_TORRENT_DIR="/mnt/pool/Data/Torrents/Data"
+
+
+TR_PATH=$TR_TORRENT_DIR'/'$TR_TORRENT_NAME
+
+#echo $TR_PATH
+
+/bin/cp -Rpl "$TR_PATH" "$COMPLETED_PATH"
